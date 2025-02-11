3cac1ee004537d65b7eb9108befa8ae626236ea7
biogolf10457 <biogolf10457@gmail.com>
2025-02-11 15:28:20 +0700

    create products api
    create products api at product.route.js
    - get, post, put, delete method by using async function from product.controller.js
    
    create function at product.controller.js
    - getProducts
    - createProduct
    - updateProduct
    - deleteProduct

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json()); // middleware that allow app to accept JSON data in req.body
+
+app.use("/api/products", productRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
