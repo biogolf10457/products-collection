afa7f89b53920cbde67429d33c18ef9acf789e92
biogolf10457 <biogolf10457@gmail.com>
2025-02-13 18:28:59 +0700

    update package.json for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,26 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // middleware that allow app to accept JSON data in req.body
 
 app.use("/api/products", productRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
